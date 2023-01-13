d368f194a59633df7fcfa285b060d8a8e857b51d
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-13 06:00:43 -0600

    creating portfolio render, nothing yet

--- react-portfolio/src/components/portfolio/index.js
+++ react-portfolio/src/components/portfolio/index.js
@@ -1,6 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../animation'
+import './index.scss'
 
-const Portfolio = () => ('bonjour')
+const Portfolio = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const [portfolio, setPortfolio] = useState([])
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            return () => clearTimeout(timer)
+        })
+    })
+
+    useEffect(() => {
+        getPortfolio()
+    }, [])
+
+    getPortfolio = (portfolio) => {
+        const querySnapshot = await getDocs(collection(db, 'portfolio'))
+        setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
+    }
+
+    const renderPortfolio = (portfolio) => {
+        return {
+            
+        }
+    }
+
+    return (
+        <>
+            <div className='container portfolio-page'>
+                <h1 className='page-title'>
+                    <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={'portfolio'.split}
+                            idx={15} 
+                        />
+                </h1>
+                <div>{renderPortfolio(portfolio)}</div>
+            </div>
+            <Loader type='pacman' />
+        </>
+    )
+}
 
 export default Portfolio
