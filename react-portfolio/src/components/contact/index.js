cc89238e91834ab68f926599ad6bea1726abebdc
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-13 03:49:35 -0600

    contact branch created, switching now

---
+++ react-portfolio/src/components/contact/index.js
@@ -0,0 +1,8 @@
+import Loader from 'react-loaders'
+
+const contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
+}
+
+const sendEmail = (e) =>
