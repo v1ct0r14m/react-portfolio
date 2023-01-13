a0855b3cf4cf0f2be080d610f59e7d54bb67c93a
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-12 22:11:03 -0600

    finally figured out why site wasnt displaying - browserrouter not defined. added nav bar. added styles

--- react-portfolio/src/components/sidebar/index.js
+++ react-portfolio/src/components/sidebar/index.js
@@ -1,13 +1,30 @@
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import initialLogo from '../../assets/images/my-logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 const Sidebar = () => (
     <div className='nav-bar'>
-        <link className='logo' to='/'>
+        <Link className='logo' to='/'>
             <img src={initialLogo} alt='logo' />
-
-        </link>
+            <h1>victoria mota</h1>
+        </Link>
+        <nav>
+            <NavLink exact='true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact='true' activeclassname='active' className='contact-link' to='/'>
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+            </NavLink>
+        </nav>
     </div>
 )
 
