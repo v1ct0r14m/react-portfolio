893eb12b5f5feb69e34e840afdd71ce27591322f
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-12 23:29:44 -0600

    added home component, will now use branches to indicate component worked on

--- react-portfolio/src/components/layout/index.js
+++ react-portfolio/src/components/layout/index.js
@@ -1,11 +1,22 @@
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../sidebar'
 import './index.scss'
 
 const Layout = () => {
     return (
-        <>  
+        <div className='app'>  
             <Sidebar/>
-        </>
+            <div className='page'>
+                <span className='tags top-tags'>&lt;body&gt;</span>
+
+                <Outlet />
+
+                <span className='tags bottom-tags'>
+                &lt;body&gt;
+                <br />
+                <span className='bottom-tag-html'/>&lt;html&gt;</span>
+            </div>
+        </div>
     )
 }
 
