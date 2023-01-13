629545cfb0928a027d146021c5ce297ff5b47134
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-12 20:48:07 -0600

    installed packages, set up some styles, and began creating components - switching to new branch

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/layout'
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+        <Route path='/' element={<Layout />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ react-portfolio/src/components/layout/index.js
@@ -0,0 +1,12 @@
+import Sidebar from '../sidebar'
+import './index.scss'
+
+const Layout = () => {
+    return (
+        <>  
+            <Sidebar/>
+        </>
+    )
+}
+
+export default Layout
