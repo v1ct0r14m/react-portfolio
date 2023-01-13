5cf910b6efe6b8808732fe8bbdf42ebd384fab43
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-13 03:44:05 -0600

    wrapping up about, will keep loader for the sake of it - it is cute

--- react-portfolio/src/components/about/index.js
+++ react-portfolio/src/components/about/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
 import AnimatedLetters from '../animation'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCss3, faGithub, faHtml5, faDev, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faCss3, faGithub, faHtml5, faJsSquare, faReact, faNodeJs } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
@@ -22,22 +22,22 @@ const About = () => {
             <div className='stage-cube-cont'>
                 <div className='cubespinner'>
                     <div className='face1'>
-                        <FontAwesomeIcon icon={faDev} color='#188f2f' />
+                        <FontAwesomeIcon icon={faNodeJs} color='#188f2f' />
                     </div>
                     <div className='face2'>
                         <FontAwesomeIcon icon={faHtml5} color='#f06529' />
                     </div>
                     <div className='face3'>
-                        <FontAwesomeIcon icon={faCss3} color='#2814d9' />
+                        <FontAwesomeIcon icon={faCss3} color='#144fd9' />
                     </div>
                     <div className='face4'>
-                        <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                        <FontAwesomeIcon icon={faReact} color='#5ed4f4' />
                     </div>
                     <div className='face5'>
                         <FontAwesomeIcon icon={faJsSquare} color='#efd81d' />
                     </div>
                     <div className='face6'>
-                        <FontAwesomeIcon icon={faGithub} color='#ec4d28' />
+                        <FontAwesomeIcon icon={faGithub} color='#2615e8' />
                     </div>
                 </div>
             </div>
--- react-portfolio/src/components/home/index.js
+++ react-portfolio/src/components/home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../animation'
 import './index.scss'
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -15,6 +16,7 @@ const Home = () => {
     //  }, [])
 
     return (
+        <>
         <div className='conatiner home-page'>
             <div className='text-zone'>
 
@@ -41,6 +43,8 @@ const Home = () => {
                 <Link to='/contact' className='flat-button'>contact me</Link>
             </div>
         </div>
+        <Loader type='pacman' />
+        </>
     )
 }
 
