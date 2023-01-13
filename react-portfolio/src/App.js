a987366b08af04e2ae4fff9f89d4385db949f6e6
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-12 21:46:16 -0600

    added small styles and sidebar - react page blank

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import './App.scss'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 
 
--- react-portfolio/src/components/sidebar/index.js
+++ react-portfolio/src/components/sidebar/index.js
@@ -1,7 +1,14 @@
 import './index.scss'
+import initialLogo from '../../assets/images/my-logo.png'
 
-const Sidebar = () => {
-    (<>hello from sidebar</>)
-}
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <link className='logo' to='/'>
+            <img src={initialLogo} alt='logo' />
+
+        </link>
+    </div>
+)
 
 export default Sidebar
