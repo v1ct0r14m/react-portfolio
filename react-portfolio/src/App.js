e2412a5f870b1fdcbb5600341edb534e5b72e6a6
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-13 03:54:40 -0600

    checking functionality of props/elements

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 import Home from './components/home'
 import About from './components/about'
+import Contact from './components/contact'
 import React, { useState } from 'react'
 
 
@@ -22,6 +23,7 @@ function App() {
           <Route path='/' element={<Layout />} >
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
           </Route>
       </Routes>
     </>
--- react-portfolio/src/components/contact/index.js
+++ react-portfolio/src/components/contact/index.js
@@ -1,8 +1,18 @@
+import './index.scss'
+import emailJs from '@emailjs/browser'
 import Loader from 'react-loaders'
 
-const contact = () => {
+
+const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const form = useRef()
 }
 
-const sendEmail = (e) =>
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailJs
+        .sendform('gmail', 'template_YeJhZkgb',)
+}
+
+export default Contact
