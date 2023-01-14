4c8e3dded9610f5cf453349703ea92a6e72f6608
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-14 00:06:14 -0600

    could not import from github/out of page db

--- react-portfolio/src/components/portfolio/index.js
+++ react-portfolio/src/components/portfolio/index.js
@@ -2,25 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../animation'
 import './index.scss'
+import { db } from '../../data'
+import { collection } from '../../data'
 
 const Portfolio = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const [portfolio, setPortfolio] = useState([])
+    const [letterClass] = useState('text-animate')
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            return () => clearTimeout(timer)
-        })
-    })
 
-    useEffect(() => {
-        getPortfolio()
-    }, [])
-
-    const getPortfolio = async () => {
-        const querySnapshot = await getDocs(collection(db, 'portfolio'))
-        setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
-    }
 
     const renderPortfolio = (portfolio) => {
         return (
@@ -43,23 +31,6 @@ const Portfolio = () => {
                 )}    
             </div>
         )    
-    }
-
-    return (
-        <>
-            <div className='container portfolio-page'>
-                <h1 className='page-title'>
-                    <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={'portfolio'.split}
-                            idx={15} 
-                        />
-                </h1>
-                <div>{renderPortfolio(portfolio)}</div>
-            </div>
-            <Loader type='pacman' />
-        </>
-    )
 }
 
 export default Portfolio
